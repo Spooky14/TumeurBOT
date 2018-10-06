f6c4f6d5087dd09062be5ac861d8f43fdca74057
Spooky14 <43904243+Spooky14@users.noreply.github.com>
2018-10-06 11:12:43 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,62 @@
+const Discord = require ('discord.js');
+const talkedRecently = new Set();
+const fs = require ('fs');
+var bot = new Discord.Client();
+
+
+bot.login('NDg3OTM3NTEyMzk3MDc4NTUw.Dna7eQ.JyB-v8gMnpUMUMUE4lCzp6hZ3cs');
+
+
+bot.on('ready',() => {
+    console.log('Bot Started!')
+    bot.user.setStatus('dnd')
+    bot.user.setActivity('🌍 Faire chier la commu 🔫')
+})
+
+
+bot.on('message', message => {
+  if (!message.guild) return;
+if (message.channel.permissionsFor(message.member).hasPermission("KICK_MEMBERS"))
+  if (message.content.startsWith('*kick')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.kick().then(() => {
+          message.channel.send(`✔️${user.tag} a bien été éjécté.`);
+        }).catch(err => {
+          message.channel.send('❌ Tu ne peut pas éjécté cette personne.');
+          console.error(err);
+        });
+      } else {
+        message.channel.send('❌ Cette personne ne peut etre éjécté');
+      }
+    } else {
+      message.channel.send('❌ Il faut mentionner une personne.');
+    }
+  }
+});
+
+bot.on('message', message => {
+  if (!message.guild) return;
+if (message.channel.permissionsFor(message.member).hasPermission("BAN_MEMBERS"))
+  if (message.content.startsWith('*ban')) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member.ban().then(() => {
+          message.channel.send(`✔️${user.tag} a bien été banni.`);
+        }).catch(err => {
+          message.channel.send('❌ Tu ne peut pas bannir cette personne.');
+          console.error(err);
+        });
+      } else {
+        message.channel.send('❌ Cette personne ne peut etre banni');
+      }
+    } else {
+      message.channel.send('❌ Il faut mentionner une personne.');
+    }
+  }
+});
+
